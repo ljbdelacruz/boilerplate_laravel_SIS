7755484ef26f17ebdf2bd7f1a3646b2479fe3448
lainel john dela cruz <ljbdelacruz123@gmail.com>
2025-04-05 20:30:07 +0800

    updates

--- scripts/deploy_app.sh
+++ scripts/deploy_app.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # build and run the containers
 docker-compose up -d --build
+
 # generate application key inside the container
 docker-compose exec app php artisan key:generate
-# run migrations
-docker-compose exec app php artisan migrate
+
+# run migrations and seeders
+docker-compose exec app php artisan migrate --seed
 
